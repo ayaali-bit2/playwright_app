2a5e79e4c6e7c2f74953b876e4d6263ea18e2a04
Vylor <vylor@vylor.ai>
2026-02-02 14:53:01 +0000

    Added `add_item_spec.ts` with test for adding item and checking if marked as checked, removed duplicate from `todos.spec.ts`.

--- tests/todos.spec.ts
+++ tests/todos.spec.ts
@@ -11,21 +11,6 @@ test.describe("todo application", ()=>{
     })
 
 
-    test("adding item and checkin if checked item or not",async ({page}) => {
-        
-        await page.locator('[data-testid="add"]').click();
-
-        await page.pause();
-
-        await page.locator('[data-testid="new-todo"]').fill("playwright course");
-        await page.locator('[data-testid="submit-newTask"]').click();
-
-        await page.locator('[data-testid="complete-task"]').nth(0).click();
-
-        const item = page.locator('[data-testid="todo-item"]').nth(0);
-        await expect(item).toHaveCSS("background-color","rgb(33, 76, 97)");
-
-    })
 
     test("checking welcome message is displayed or not", async ({page}) =>{
 
