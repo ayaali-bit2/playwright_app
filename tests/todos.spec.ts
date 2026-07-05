5fa42152726494b45f316c210a13edac482012f2
Vylor <vylor@vylor.ai>
2026-07-05 10:49:02 +0000

    test: add todo helpers and create-todo test

--- tests/todos.spec.ts
+++ tests/todos.spec.ts
@@ -14,6 +14,17 @@ async function verifyWelcomeMessage(page: Page) {
     await expect(welcomeMessage).toBeVisible();
 }
 
+// Create a new todo
+async function createTodo(page: Page, item: string) {
+    await page.locator('[data-testid="add"]').fill(item);
+    await page.locator('[data-testid="submit-newTask"]').click();
+}
+
+// Verify a todo exists
+async function verifyTodoExists(page: Page, item: string) {
+    await expect(page.locator(`text=${item}`)).toBeVisible();
+}
+
 test.describe("todo application", () => {
 
     test.use({
@@ -27,6 +38,13 @@ test.describe("todo application", () => {
     test("checking welcome message is displayed or not", async ({ page }) => {
         await page.pause();
         await verifyWelcomeMessage(page);
+    });
+
+    test("should create a new todo", async ({ page }) => {
+        const todo = "Learn Playwright";
+
+        await createTodo(page, todo);
+        await verifyTodoExists(page, todo);
     });
 
 });
